d580b48554b4501620188f6d655a12f32c9995aa
xin.chong <xin.chong@aispeech.com>
2022-04-17 12:30:53 +0800

    init

---
+++ src/test/java/com/pdool/chromews/ChromeWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdool.chromews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChromeWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
